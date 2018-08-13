ed0649c06dff76a0e977a959ed9341ad817e616d
lubamark <lubamark@mail.ru>
2018-08-13 20:55:05 +0700

    1440

--- source/js/scripts/slider.js
+++ source/js/scripts/slider.js
@@ -1,5 +1,31 @@
 $('.j-slider').slick({
+  mobileFirst: true,
+  infinite: true,
   appendArrows: $('.j-slider-controls'),
   prevArrow: '<button class="button slider__control slider__control--prev"><span class="visually-hidden">Предыдущее фото</span></button>',
-  nextArrow: '<button class="button slider__control slider__control--next"><span class="visually-hidden">Следующее фото</span></button>'
+  nextArrow: '<button class="button slider__control slider__control--next"><span class="visually-hidden">Следующее фото</span></button>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 5,
+        centerMode: false,
+        variableWidth: true,
+        speed: 800,
+        lazyLoaded: true,
+        adaptiveHeight: true
+      }
+    },
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 5,
+        centerMode: false,
+        variableWidth: true,
+        speed: 800,
+        adaptiveHeight: true
+      }
+    }
+  ]
+
 });
