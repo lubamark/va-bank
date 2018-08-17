d144d6a44f0318f3e9582deeaaf26ffe18b5f747
lubamark <lubamark@mail.ru>
2018-08-17 19:35:58 +0700

    add news page

--- build/js/all.js
+++ build/js/all.js
@@ -4378,6 +4378,28 @@ Overlay.hide = function ()
 };
 /* /Overlay */
 
+// $(document).ready(function () {
+//   var newsItem = $('.j-news-item');
+//   var mainClass = 'news__main';
+//   var windowWidth = $(window).width();
+//   var toggleMain = function () {
+//     windowWidth = $(window).width();
+//     if(windowWidth > 1024) {
+//       newsItem.addClass(mainClass);
+//     } else {
+//       newsItem.removeClass(mainClass);
+//     }
+//   };
+//
+//   $(window).on('load', function () {
+//     toggleMain();
+//   });
+//
+//   $(window).resize(function () {
+//       toggleMain();
+//     });
+// });
+
 $('.j-select').selectric({
   nativeOnMobile: false
 });
